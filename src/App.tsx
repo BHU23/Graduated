e2b2e38431460722e85b2360480aefbfcd9ed0a1
Bhuwadol <125109678+BHU23@users.noreply.github.com>
2024-07-15 21:45:12 +0700

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import Header from "./componet/header";
 import Footer from "./componet/footer";
 import { Box, Toolbar } from "@mui/material";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GradePoint from "./pages/GradePoint";
 import GPA from "./pages/GPA";
 import GPAX from "./pages/GPAX";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Box
         sx={{
           width: "100%",
@@ -28,8 +28,8 @@ export default function App() {
             <Route path="/GPAX" element={<GPAX />} />
           </Routes>
         </Box>
-        
-      </Box><Footer />
-    </BrowserRouter>
+      </Box>
+      <Footer />
+    </Router>
   );
 }
